image normalization fixed; test added

loaders/features.py
@@ -6,8 +6,8 @@ import pandas as pd
 import pickle
 import torch
 
-PATH_TO_TABULAR = 'datasets/participants.tsv'
-PATH_TO_IMAGES = 'datasets/gstore/scratch/u/iriondoc/BHB_data/midaxial_2D'
+PATH_TO_TABULAR = '../datasets/participants.tsv'
+PATH_TO_IMAGES = '../datasets/gstore/scratch/u/iriondoc/BHB_data/midaxial_2D'
 
 # each sample is a list consisting of [obs_data_dict, image]
 class CausalEmbeddingsDataset(torch.utils.data.Dataset):
@@ -33,7 +33,9 @@ class CausalEmbeddingsDataset(torch.utils.data.Dataset):
     }
 
     def __init__(self):
+        dirname = os.path.dirname(__file__)
         tab_path = PATH_TO_TABULAR
+        tab_path = os.path.join(dirname, tab_path)
         participants = pd.read_csv(tab_path, sep='\t')
 
         # filter stages
@@ -54,40 +56,40 @@ class CausalEmbeddingsDataset(torch.utils.data.Dataset):
             df = participants[col_name]
             _min, _max = df.min(), df.max()
             df = (df - _min) / (_max - _min)
-            self.column_range[col_name] = (_min, max)
+            self.column_range[col_name] = (_min, _max)
             dfs.append(df)
 
         dfs.append(participants['participant_id'])
         self.tabular_df = pd.concat(dfs, axis=1)
         self.column_range['image'] = self.get_image_normalization_coefficients()
 
-        with open("logs/preprocessing.pkl", "wb") as handle:
+        with open("../logs/preprocessing.pkl", "wb") as handle:
             pickle.dump(self.column_range, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
         self.schema = {key: len(value) for key, value in self.param_groups.items()}
 
     def get_image_normalization_coefficients(self):
-        min_val, max_val = torch.inf, -torch.inf
-
+        min_max_dict = {}
         for id in self.tabular_df['participant_id']:
             img = self.get_original_image(id)
-            min_val = min(min_val, img.min())
-            max_val = max(max_val, img.max())
+            min_max_dict[id] = (img.min(), img.max())
 
-        return (min_val, max_val)
+        return min_max_dict
 
     def get_original_image(self, id):
         image_name = "sub-" + str(id) + '_preproc-quasiraw_T1w.npy'
-        image_path = os.path.join(PATH_TO_IMAGES, image_name)
+        dirname = os.path.dirname(__file__)
+        image_path = os.path.join(dirname, PATH_TO_IMAGES, image_name)
         image = np.load(image_path)
         image = cv2.resize(image, (32,32))
         return image
 
     @functools.lru_cache(maxsize=250, typed=False)
-    def get_transformed_image(self, idx):
+    def get_transformed_image(self, idx, id):
         image_name = "sub-" + str(int(self.tabular_df.iloc[idx]['participant_id'])) + '_preproc-quasiraw_T1w.npy'
-        image_path = os.path.join(PATH_TO_IMAGES, image_name)
-        min_val, max_val = self.column_range['image'][0], self.column_range['image'][1]
+        dirname = os.path.dirname(__file__)
+        image_path = os.path.join(dirname, PATH_TO_IMAGES, image_name)
+        min_val, max_val = self.column_range['image'][id][0], self.column_range['image'][id][1]
         _range = max_val - min_val
         image = np.load(image_path)
         image = cv2.resize(image, (32,32))
@@ -99,7 +101,8 @@ class CausalEmbeddingsDataset(torch.utils.data.Dataset):
         return len(self.tabular_df)
 
     def __getitem__(self, idx):
-        image = self.get_transformed_image(idx)
+        id = self.tabular_df.iloc[idx]['participant_id']
+        image = self.get_transformed_image(idx, id)
         tab_values = self.tabular_df.drop(columns=['participant_id']).iloc[idx]
 
         final_tab_values = {}
@@ -107,14 +110,14 @@ class CausalEmbeddingsDataset(torch.utils.data.Dataset):
             consolidated_vec = torch.tensor([tab_values[column] for column in group]).float()
             final_tab_values[name] = consolidated_vec
 
-        return torch.tensor(image, dtype=torch.float), final_tab_values
+        return torch.tensor(image, dtype=torch.float), final_tab_values, id
 
 
 if __name__ == '__main__':
     dataset = CausalEmbeddingsDataset()
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
 
-    for imgs, labels in data_loader:
+    for imgs, labels, _ in data_loader:
         print("Batch of images has shape: ", imgs.shape)
         for label, mat in labels.items():
             print(label)

models/models.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     conv_ae = ConvolutionalAE(dataset.schema)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
 
-    for images, obs_dict in data_loader:
+    for images, obs_dict, _ in data_loader:
         res = conv_ae(obs_dict, images)
 
 

train.py
@@ -20,7 +20,7 @@ def test(model, test_loader):
     test_loss = 0.
     for sample in test_loader:
         with torch.no_grad():
-            images, labels = sample
+            images, labels, _ = sample
             pred_images, image_emb = model(images)
             test_loss += loss_fn(images, pred_images)
 
@@ -33,7 +33,7 @@ def train(model, optimizer, train_loader, epoch, batch_size):
     loss_fn = torch.nn.MSELoss()
     for batch_idx, sample in enumerate(train_loader):
         optimizer.zero_grad()
-        images, obs_dict = sample
+        images, obs_dict, _ = sample
         pred_images, image_emb = model(images)
         loss = loss_fn(images, pred_images)
         #loss = compute_total_loss(images, obs_dict, model)
@@ -67,18 +67,18 @@ if __name__ == '__main__':
 
     optimizer = torch.optim.Adam(model.parameters())
 
-    best_loss = -100.
+    best_loss = 100.
     start_time = time.time()
     for epoch in range(num_epochs):
         train(model, optimizer, train_loader, epoch, batch_size)
         loss = test(model, test_loader)
         is_best = loss > best_loss
-        best_loss = max(loss, best_loss)
+        best_loss = min(loss, best_loss)
         save_checkpoint({
             'epoch': epoch + 1,
             'state_dict': model.state_dict(),
             'best_loss': best_loss,
             'optimizer': optimizer.state_dict()
-        }, is_best, filepath='../logs')
+        }, is_best, filepath='logs')
 
 